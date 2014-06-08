a79dc9600b62d25a09485c16e42b31233befc3da
and <NORTHAMERICA\and>
2014-06-07 20:24:58 -0400

    spelling issues in part1

--- plot1.R
+++ plot1.R
@@ -2,5 +2,5 @@ data <- read.table("data//household_power_consumption.txt", header=TRUE, sep=";"
 head(data)
 TwoDaydata<-data[data$Date == "1/2/2007" | data$Date ==  "2/2/2007",]
 png(filename="plot1.png",width=480,height=480,units="px")
-hist(TwoDaydata$Global_active_power,main="Global Active Power", xlab="Global Active Power (Killowats)",col="red")
+hist(TwoDaydata$Global_active_power,main="Global Active Power", xlab="Global Active Power (Kilowatts)",col="red")
 dev.off()
