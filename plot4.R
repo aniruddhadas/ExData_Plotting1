5ca237b10e79e1e6a5bedb6c2e3ff9d31ba2e8d3
and <NORTHAMERICA\and>
2014-06-07 20:57:28 -0400

    issues with plot4.R

--- plot4.R
+++ plot4.R
@@ -6,12 +6,12 @@ png(filename="plot4.png",width=480,height=480,units="px")
 par(mfrow = c(2, 2))
 plot(TwoDaydata$DateTime, TwoDaydata$Global_active_power, type="l", xlab="", ylab="Global Active Power")
 
-plot(TwoDaydata$DateTime, TwoDaydata$Voltage, type="l", xlab="", ylab="Voltage")
+plot(TwoDaydata$DateTime, TwoDaydata$Voltage, type="l", xlab="datetime", ylab="Voltage")
 
 plot(TwoDaydata$DateTime, TwoDaydata$Sub_metering_1, type="l", xlab="", ylab="Energy sub metering")
 lines(TwoDaydata$DateTime, TwoDaydata$Sub_metering_2, type="l", col="red")
 lines(TwoDaydata$DateTime, TwoDaydata$Sub_metering_3, type="l", col="blue")
 legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend=c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'))
 
-plot(TwoDaydata$DateTime, TwoDaydata$Global_reactive_power, type="l", xlab="", ylab="Global_reactive_power")
+plot(TwoDaydata$DateTime, TwoDaydata$Global_reactive_power, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
